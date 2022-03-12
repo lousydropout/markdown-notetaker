9a086ec34a6f841fa1e0b52d5b221c865724f4c6
Vincent Chan <vincent@doingcloudstuff.com>
2022-03-12 12:53:17 -0800

    convert example notebook.json to js file

---
+++ assets/notebook.js
@@ -0,0 +1,21 @@
+export const notebookData = {
+  filename: "notebook.json",
+  data: [
+    {
+      id: "1",
+      content_type: "markdown",
+      content:
+        "# My Heading\nThe quick red fox jumped over the lazy brown dog.",
+    },
+    {
+      id: "2",
+      content_type: "markdown",
+      content: "This should be in cell 2.",
+    },
+    {
+      id: "3",
+      content_type: "markdown",
+      content: "This should be in cell 3.",
+    },
+  ],
+};
