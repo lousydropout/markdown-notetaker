56001c318f1bbd48274355456398c78576459d80
Vincent Chan <vincent@doingcloudstuff.com>
2022-03-24 19:39:32 -0700

    update Cell component (use display rather than conditional render)

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -12,7 +12,8 @@ const Cell = ({ id, content, updateData, content_type: contentType }) => {
   const [editMode, setEditMode] = useState(false);
   const [ready, setReady] = useState(false);
 
-  const callback = () => {
+  const toggleEditMode = () => {
+    console.log("toggling");
     setEditMode((editMode) => !editMode);
   };
 
@@ -22,88 +23,63 @@ const Cell = ({ id, content, updateData, content_type: contentType }) => {
 
   useEffect(() => setReady(true), [content]);
 
-  useEffect(() => {
-    if (ready) {
-      document
-        .getElementById(cellEditBtnId)
-        .addEventListener("click", callback);
-    }
-    return () => {
-      document
-        .getElementById(cellEditBtnId)
-        .removeEventListener("click", callback);
-    };
-  }, [ready, cellEditBtnId]);
-
+  if (!ready) {
+    return <div>Loading</div>;
+  }
   return (
     <>
-      {ready || <div>Loading</div>}
-      {ready && (
-        <div className="cell-bar">
-          {/* <span> Mode: {editMode ? "Edit" : "Presentation"}</span> */}
-          <button className="toggle-edit-mode-btn" id={cellEditBtnId}>
-            {editMode || (
-              <>
-                <a className="edit-mode-toggle-btn" href={`#${cellEditId}`}>
-                  <FontAwesomeIcon
-                    icon={faArrowsRotate}
-                    className="toggleIcon"
-                  />
-                  <span className="toggleBtnText">
-                    {editMode ? "Edit" : "Presentation"} mode
-                  </span>
-                </a>
-              </>
-            )}
-            {editMode && (
-              <>
-                <FontAwesomeIcon icon={faArrowsRotate} className="toggleIcon" />
-                <span className="toggleBtnText">
-                  {editMode ? "Edit" : "Presentation"} mode
-                </span>
-              </>
-            )}
-          </button>
-        </div>
-      )}
-      {ready && editMode && (
-        <textarea
-          className="cell-edit-mode"
-          value={content}
-          onChange={handleText}
-          id={cellEditId}
-          autoFocus={true}
-          onFocus={(e) => (e.target.selectionStart = e.target.value.length)}
+      <div className="cell-bar">
+        <button
+          className="toggle-edit-mode-btn"
+          id={cellEditBtnId}
+          onClick={toggleEditMode}
+        >
+          <a className="edit-mode-toggle-btn" href={`#${cellEditId}`}>
+            <FontAwesomeIcon icon={faArrowsRotate} className="toggleIcon" />
+            <span className="toggleBtnText">
+              {editMode ? "Edit" : "Presentation"} mode
+            </span>
+          </a>
+        </button>
+      </div>
+      <textarea
+        style={{ display: editMode ? "block" : "none" }}
+        className="cell-edit-mode"
+        value={content}
+        onChange={handleText}
+        id={cellEditId}
+        autoFocus={true}
+        onFocus={(e) => (e.target.selectionStart = e.target.value.length)}
+      />
+      <div
+        className="reactMarkdown"
+        id={reactMarkdownId}
+        style={{ display: editMode ? "none" : "block" }}
+        onDoubleClick={toggleEditMode}
+      >
+        <ReactMarkdown
+          children={content}
+          className="markdown cell-presentation-mode"
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
         />
-      )}
-
-      {ready &&
-        (editMode || (
-          <div className="reactMarkdown" id={reactMarkdownId}>
-            <ReactMarkdown
-              children={content}
-              className="markdown cell-presentation-mode"
-              remarkPlugins={[remarkGfm]}
-              components={{
-                code({ node, inline, className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || "");
-                  return !inline && match ? (
-                    <SyntaxHighlighter
-                      children={String(children).replace(/\n$/, "")}
-                      language={match[1]}
-                      PreTag="div"
-                      {...props}
-                    />
-                  ) : (
-                    <code className={className} {...props}>
-                      {children}
-                    </code>
-                  );
-                },
-              }}
-            />
-          </div>
-        ))}
+      </div>
     </>
   );
 };
